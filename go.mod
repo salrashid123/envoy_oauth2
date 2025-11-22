c3ac462d490f34fa118eff76061b30a75cc2bfd3
sal rashid <salrashid123@gmail.com>
2025-11-21 22:51:40 -0500

    update server for id_token
    Signed-off-by: sal rashid <salrashid123@gmail.com>

--- go.mod
+++ go.mod
@@ -1,10 +1,39 @@
 module main
 
-go 1.16
+go 1.24.0
 
 require (
+	cloud.google.com/go/auth v0.17.0
+	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/gorilla/mux v1.8.0
-	golang.org/x/net v0.0.0-20211209124913-491a49abca63
-	google.golang.org/api v0.63.0 // indirect
-	google.golang.org/grpc v1.43.0
+	github.com/lestrrat/go-jwx v0.9.1
+	golang.org/x/net v0.43.0
+	golang.org/x/oauth2 v0.30.0
+	google.golang.org/api v0.239.0
+)
+
+require (
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.8.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
+	github.com/lestrrat/go-pdebug v0.0.0-20180220043741-569c97477ae8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
+	go.opentelemetry.io/otel v1.36.0 // indirect
+	go.opentelemetry.io/otel/metric v1.36.0 // indirect
+	go.opentelemetry.io/otel/trace v1.36.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822 // indirect
+	google.golang.org/grpc v1.74.2 // indirect
+	google.golang.org/protobuf v1.36.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -10,7 +12,10 @@ import (
 
 	"flag"
 
+	"cloud.google.com/go/auth/credentials/idtoken"
+	jwt "github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
+	"github.com/lestrrat/go-jwx/jwk"
 	"golang.org/x/net/http2"
 	"golang.org/x/oauth2"
 	oauth2svc "google.golang.org/api/oauth2/v2"
@@ -21,13 +26,20 @@ var (
 	validateUser = flag.Bool("validateUser", false, "Lookup User with oauth2 tokeninfo endpoint")
 )
 
+var (
+	jwtSet *jwk.Set
+)
+
 const (
 	hmacKey = "93Wg15rHSp6/Si5bH756OE6mAqL9ntX5DQ7ug5NgncE="
 )
 
 type contextKey string
 
-const contextEventKey contextKey = "event"
+const (
+	audience                   = "839905111702-vrh3m9blh9d7m25dj2cs8fv2v5qle3oa.apps.googleusercontent.com"
+	contextEventKey contextKey = "event"
+)
 
 type parsedData struct {
 	AccessToken string `json:"access_token"`
@@ -45,8 +57,44 @@ func oauthMiddleware(h http.Handler) http.Handler {
 
 		fmt.Printf("Headers: %s\n", dump)
 
-		accessToken := ""
+		ctx := context.Background()
 
+		// extract and validate id_token
+		idToken := ""
+		idTokenCookie, err := r.Cookie("IdToken")
+		if err != nil {
+			http.Error(w, "IdToken not present", http.StatusUnauthorized)
+			return
+		}
+		idToken = idTokenCookie.Value
+
+		//fmt.Printf("idToken: %s\n", idToken)
+
+		validTok, err := idtoken.Validate(ctx, idToken, audience)
+		if err != nil {
+			log.Fatalf("token validation failed: %v", err)
+		}
+		if validTok.Audience != audience {
+			log.Fatalf("got %q, want %q", validTok.Audience, audience)
+		}
+
+		// If you want to verify any other issuer, first get the JWK endpoint,
+		// in the following we are validating google's tokens, meaning its equivalent to the bit above
+		// this is added in as an example of verifying IAP or other token types
+		jwksURL := "https://www.googleapis.com/oauth2/v3/certs"
+		jwtSet, err = jwk.FetchHTTP(jwksURL)
+		if err != nil {
+			log.Fatal("Unable to load JWK Set: ", err)
+		}
+		doc, err := verifyGoogleIDToken(ctx, idToken)
+		if err != nil {
+			log.Fatalf("Unable to verify IDTOKEN: %v", err)
+		}
+		log.Printf("Verified Token with subject: %v", doc.Email)
+
+		// use access token against an API, in this case, it happens to be the oauth api
+
+		accessToken := ""
 		bearerTokenCookie, err := r.Cookie("BearerToken")
 		if err != nil {
 			http.Error(w, "BearerToken not present", http.StatusUnauthorized)
@@ -54,19 +102,7 @@ func oauthMiddleware(h http.Handler) http.Handler {
 		}
 		accessToken = bearerTokenCookie.Value
 
-		//		fmt.Printf("accessToken: %s\n", accessToken)
-
-		// idToken := ""
-		// idTokenCookie, err := r.Cookie("IdToken")
-		// if err != nil {
-		// 	http.Error(w, "IdToken not present", http.StatusUnauthorized)
-		// 	return
-		// }
-		// idToken = idTokenCookie.Value
-
-		// fmt.Printf("idToken: %s\n", idToken)
-
-		ctx := context.Background()
+		//	fmt.Printf("accessToken: %s\n", accessToken)
 
 		rootTS := oauth2.StaticTokenSource(&oauth2.Token{
 			AccessToken: accessToken,
@@ -122,4 +158,46 @@ func main() {
 	err := server.ListenAndServeTLS("certs/backend.crt", "certs/backend.key")
 	fmt.Printf("Unable to start Server %v", err)
 
+}
+
+type gcpIdentityDoc struct {
+	Google struct {
+		ComputeEngine struct {
+			InstanceCreationTimestamp int64  `json:"instance_creation_timestamp,omitempty"`
+			InstanceID                string `json:"instance_id,omitempty"`
+			InstanceName              string `json:"instance_name,omitempty"`
+			ProjectID                 string `json:"project_id,omitempty"`
+			ProjectNumber             int64  `json:"project_number,omitempty"`
+			Zone                      string `json:"zone,omitempty"`
+		} `json:"compute_engine"`
+	} `json:"google"`
+	Email           string `json:"email,omitempty"`
+	EmailVerified   bool   `json:"email_verified,omitempty"`
+	AuthorizedParty string `json:"azp,omitempty"`
+	jwt.RegisteredClaims
+}
+
+func getKey(token *jwt.Token) (interface{}, error) {
+	keyID, ok := token.Header["kid"].(string)
+	if !ok {
+		return nil, errors.New("expecting JWT header to have string kid")
+	}
+	if key := jwtSet.LookupKeyID(keyID); len(key) == 1 {
+		log.Printf("     Found OIDC KeyID  " + keyID)
+		return key[0].Materialize()
+	}
+	return nil, errors.New("unable to find key")
+}
+
+func verifyGoogleIDToken(ctx context.Context, rawToken string) (gcpIdentityDoc, error) {
+	token, err := jwt.ParseWithClaims(rawToken, &gcpIdentityDoc{}, getKey)
+	if err != nil {
+		log.Printf("     Error parsing JWT %v", err)
+		return gcpIdentityDoc{}, err
+	}
+	if claims, ok := token.Claims.(*gcpIdentityDoc); ok && token.Valid {
+		log.Printf("     OIDC doc has Audience [%s]   Issuer [%s] and SubjectEmail [%s]", claims.Audience, claims.RegisteredClaims.Issuer, claims.Email)
+		return *claims, nil
+	}
+	return gcpIdentityDoc{}, errors.New("Error parsing JWT Claims")
 }
